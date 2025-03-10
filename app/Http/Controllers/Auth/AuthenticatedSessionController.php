20e35aa0f5a287ff6a990ae33fac4f04716e781f
rian1504 <adaadi3526@gmail.com>
2025-03-10 15:46:09 +0700

    Feat: API Authentication dengan Breeze Menyelesaikan semua fitur Authentication dan Tersisa fitur Verifikasi Email

---
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+
+class AuthenticatedSessionController extends Controller
+{
+    /**
+     * Handle an incoming authentication request.
+     */
+    public function store(LoginRequest $request): JsonResponse
+    {
+        $request->authenticate();
+
+        $user = $request->user();
+
+        $user->tokens()->delete();
+
+        // Membuat token
+        $userToken = $user->createToken('user-token')->plainTextToken;
+
+        // return data
+        return response()->json([
+            'user' => $user,
+            'token' => $userToken,
+            'message' => 'Berhasil Login!',
+        ]);
+    }
+
+    /**
+     * Destroy an authenticated session.
+     */
+    public function destroy(Request $request): JsonResponse
+    {
+        Auth::guard('web')->logout();
+
+        // Menghapus token
+        $request->user()->tokens()->delete();
+
+        // return respons
+        return response()->json([
+            'message' => 'Berhasil Logout!',
+        ]);
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -3,27 +3,34 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+use Filament\Panel;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Notifications\Notifiable;
+use Filament\Models\Contracts\FilamentUser;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable implements FilamentUser, MustVerifyEmail
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
      *
      * @var list<string>
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-        'avatar',
-    ];
+    // protected $fillable = [
+    //     'name',
+    //     'email',
+    //     'password',
+    //     'avatar',
+    // ];
+
+    protected $guarded = ['id'];
 
     /**
      * The attributes that should be hidden for serialization.
@@ -46,6 +53,16 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        $panel_id = $panel->getId();
+        if ($panel_id === 'admin') {
+            return $this->is_admin == 1;
+        } else {
+            return false;
+        }
     }
 
     // Define the relationship
