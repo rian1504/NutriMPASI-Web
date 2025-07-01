38a3a629167c9fba0fe1cbd903c31973ec235bf0
rian1504 <adaadi3526@gmail.com>
2025-07-01 17:53:04 +0700

    feat(route testing)
    - disable auto fill login
    - add route testing
    - update route to use version

--- routes/api.php
+++ routes/api.php
@@ -19,79 +19,161 @@ use App\Http\Controllers\Api\FoodSuggestionController;
 require __DIR__ . '/auth.php';
 
 Route::group(['middleware' => 'auth:sanctum'], function () {
-    // Food
-    Route::prefix('food')->group(function () {
-        Route::get('', [FoodController::class, 'index']);
-        Route::get('category', [FoodController::class, 'category']);
-        Route::post('filter', [FoodController::class, 'filter']);
-        Route::get('record', [FoodController::class, 'foodRecord']);
-        Route::get('{food}', [FoodController::class, 'show']);
-        Route::get('{food}/cook', [FoodController::class, 'showCookingGuide']);
-        Route::post('{food}/cook/complete', [FoodController::class, 'completeCooking']);
+    // v1
+    // Route::prefix('v1')->group(function () {
+    //     // Food
+    //     Route::prefix('food')->group(function () {
+    //         Route::get('', [FoodController::class, 'index']);
+    //         Route::get('category', [FoodController::class, 'category']);
+    //         Route::post('filter', [FoodController::class, 'filter']);
+    //         Route::get('record', [FoodController::class, 'foodRecord']);
+    //         Route::get('{food}', [FoodController::class, 'show']);
+    //         Route::get('{food}/cook', [FoodController::class, 'showCookingGuide']);
+    //         Route::post('{food}/cook/complete', [FoodController::class, 'completeCooking']);
+    //     });
+
+    //     // Favorite Food
+    //     Route::prefix('favorite')->group(function () {
+    //         Route::get('', [FavoriteController::class, 'index']);
+    //         Route::post('{food}', [FavoriteController::class, 'store']);
+    //     });
+
+    //     // Schedule
+    //     Route::prefix('schedule')->group(function () {
+    //         Route::get('', [ScheduleController::class, 'index']);
+    //         Route::post('filter', [ScheduleController::class, 'filter']);
+    //         Route::post('{food}', [ScheduleController::class, 'store']);
+    //         Route::get('{schedule}/edit', [ScheduleController::class, 'edit']);
+    //         Route::post('{schedule}/update', [ScheduleController::class, 'update']);
+    //         Route::delete('{schedule}', [ScheduleController::class, 'destroy']);
+    //     });
+
+    //     // Nutritionist
+    //     Route::get('nutritionist', [NutritionistController::class, 'index']);
+
+    //     // Food Suggestion
+    //     Route::resource('food-suggestion', FoodSuggestionController::class)->except(['create', 'edit', 'update']);
+    //     Route::post('food-suggestion/{food_suggestion}', [FoodSuggestionController::class, 'update'])->name('food_recommendation.update');
+
+    //     // Baby
+    //     Route::resource('baby', BabyController::class)->except(['create', 'edit', 'update']);
+    //     Route::post('baby/{baby}', [BabyController::class, 'update'])->name('baby.update');
+    //     Route::get('baby/food-recommendation/{baby}', [BabyController::class, 'foodRecommendation'])->name('baby.food-recommendation');
+
+    //     // Thread User
+    //     Route::resource('thread-user', ThreadUserController::class)->except(['create', 'show', 'edit', 'update']);
+    //     Route::post('thread-user/{thread_user}', [ThreadUserController::class, 'update'])->name('thread.update');
+
+    //     // Thread
+    //     Route::prefix('thread')->group(function () {
+    //         Route::get('', [ThreadController::class, 'index']);
+    //         Route::get('{thread}', [ThreadController::class, 'show']);
+    //     });
+
+    //     // Like Thread
+    //     Route::prefix('like')->group(function () {
+    //         Route::get('', [LikeController::class, 'index']);
+    //         Route::post('{thread}', [LikeController::class, 'store']);
+    //     });
+
+    //     // Comment
+    //     Route::resource('comment', CommentController::class)->except(['index', 'create', 'show', 'edit', 'update']);
+    //     Route::post('comment/{comment}', [CommentController::class, 'update'])->name('comment.update');
+
+    //     // Notification
+    //     Route::prefix('notification')->group(function () {
+    //         Route::get('', [NotificationController::class, 'index']);
+    //         Route::post('', [NotificationController::class, 'readAll']);
+    //         Route::post('{notification}', [NotificationController::class, 'read']);
+    //     });
+
+    //     // Profil
+    //     Route::prefix('profile')->group(function () {
+    //         Route::get('', [ProfileController::class, 'index']);
+    //         Route::post('{user}', [ProfileController::class, 'updateProfile']);
+    //         Route::post('{user}/password', [ProfileController::class, 'updatePassword']);
+    //     });
+
+    //     // Report
+    //     Route::post('report/{category}', [ReportController::class, 'store']);
+    // });
+
+    // TESTING
+    Route::prefix('testing')->group(function () {
+        // Food
+        Route::prefix('food')->group(function () {
+            Route::get('', [FoodController::class, 'index']);
+            Route::get('category', [FoodController::class, 'category']);
+            Route::post('filter', [FoodController::class, 'filter']);
+            Route::get('record', [FoodController::class, 'foodRecord']);
+            Route::get('{food}', [FoodController::class, 'show']);
+            Route::get('{food}/cook', [FoodController::class, 'showCookingGuide']);
+            Route::post('{food}/cook/complete', [FoodController::class, 'completeCooking']);
+        });
+
+        // Favorite Food
+        Route::prefix('favorite')->group(function () {
+            Route::get('', [FavoriteController::class, 'index']);
+            Route::post('{food}', [FavoriteController::class, 'store']);
+        });
+
+        // Schedule
+        Route::prefix('schedule')->group(function () {
+            Route::get('', [ScheduleController::class, 'index']);
+            Route::post('filter', [ScheduleController::class, 'filter']);
+            Route::post('{food}', [ScheduleController::class, 'store']);
+            Route::get('{schedule}/edit', [ScheduleController::class, 'edit']);
+            Route::post('{schedule}/update', [ScheduleController::class, 'update']);
+            Route::delete('{schedule}', [ScheduleController::class, 'destroy']);
+        });
+
+        // Nutritionist
+        Route::get('nutritionist', [NutritionistController::class, 'index']);
+
+        // Food Suggestion
+        Route::resource('food-suggestion', FoodSuggestionController::class)->except(['create', 'edit', 'update']);
+        Route::post('food-suggestion/{food_suggestion}', [FoodSuggestionController::class, 'update'])->name('food_recommendation.update');
+
+        // Baby
+        Route::resource('baby', BabyController::class)->except(['create', 'edit', 'update']);
+        Route::post('baby/{baby}', [BabyController::class, 'update'])->name('baby.update');
+        Route::get('baby/food-recommendation/{baby}', [BabyController::class, 'foodRecommendation'])->name('baby.food-recommendation');
+
+        // Thread User
+        Route::resource('thread-user', ThreadUserController::class)->except(['create', 'show', 'edit', 'update']);
+        Route::post('thread-user/{thread_user}', [ThreadUserController::class, 'update'])->name('thread.update');
+
+        // Thread
+        Route::prefix('thread')->group(function () {
+            Route::get('', [ThreadController::class, 'index']);
+            Route::get('{thread}', [ThreadController::class, 'show']);
+        });
+
+        // Like Thread
+        Route::prefix('like')->group(function () {
+            Route::get('', [LikeController::class, 'index']);
+            Route::post('{thread}', [LikeController::class, 'store']);
+        });
+
+        // Comment
+        Route::resource('comment', CommentController::class)->except(['index', 'create', 'show', 'edit', 'update']);
+        Route::post('comment/{comment}', [CommentController::class, 'update'])->name('comment.update');
+
+        // Notification
+        Route::prefix('notification')->group(function () {
+            Route::get('', [NotificationController::class, 'index']);
+            Route::post('', [NotificationController::class, 'readAll']);
+            Route::post('{notification}', [NotificationController::class, 'read']);
+        });
+
+        // Profil
+        Route::prefix('profile')->group(function () {
+            Route::get('', [ProfileController::class, 'index']);
+            Route::post('{user}', [ProfileController::class, 'updateProfile']);
+            Route::post('{user}/password', [ProfileController::class, 'updatePassword']);
+        });
+
+        // Report
+        Route::post('report/{category}', [ReportController::class, 'store']);
     });
-
-    // Favorite Food
-    Route::prefix('favorite')->group(function () {
-        Route::get('', [FavoriteController::class, 'index']);
-        Route::post('{food}', [FavoriteController::class, 'store']);
-    });
-
-    // Schedule
-    Route::prefix('schedule')->group(function () {
-        Route::get('', [ScheduleController::class, 'index']);
-        Route::post('filter', [ScheduleController::class, 'filter']);
-        Route::post('{food}', [ScheduleController::class, 'store']);
-        Route::get('{schedule}/edit', [ScheduleController::class, 'edit']);
-        Route::post('{schedule}/update', [ScheduleController::class, 'update']);
-        Route::delete('{schedule}', [ScheduleController::class, 'destroy']);
-    });
-
-    // Nutritionist
-    Route::get('nutritionist', [NutritionistController::class, 'index']);
-
-    // Food Suggestion
-    Route::resource('food-suggestion', FoodSuggestionController::class)->except(['create', 'edit', 'update']);
-    Route::post('food-suggestion/{food_suggestion}', [FoodSuggestionController::class, 'update'])->name('food_recommendation.update');
-
-    // Baby
-    Route::resource('baby', BabyController::class)->except(['create', 'edit', 'update']);
-    Route::post('baby/{baby}', [BabyController::class, 'update'])->name('baby.update');
-    Route::get('baby/food-recommendation/{baby}', [BabyController::class, 'foodRecommendation'])->name('baby.food-recommendation');
-
-    // Thread User
-    Route::resource('thread-user', ThreadUserController::class)->except(['create', 'show', 'edit', 'update']);
-    Route::post('thread-user/{thread_user}', [ThreadUserController::class, 'update'])->name('thread.update');
-
-    // Thread
-    Route::prefix('thread')->group(function () {
-        Route::get('', [ThreadController::class, 'index']);
-        Route::get('{thread}', [ThreadController::class, 'show']);
-    });
-
-    // Like Thread
-    Route::prefix('like')->group(function () {
-        Route::get('', [LikeController::class, 'index']);
-        Route::post('{thread}', [LikeController::class, 'store']);
-    });
-
-    // Comment
-    Route::resource('comment', CommentController::class)->except(['index', 'create', 'show', 'edit', 'update']);
-    Route::post('comment/{comment}', [CommentController::class, 'update'])->name('comment.update');
-
-    // Notification
-    Route::prefix('notification')->group(function () {
-        Route::get('', [NotificationController::class, 'index']);
-        Route::post('', [NotificationController::class, 'readAll']);
-        Route::post('{notification}', [NotificationController::class, 'read']);
-    });
-
-    // Profil
-    Route::prefix('profile')->group(function () {
-        Route::get('', [ProfileController::class, 'index']);
-        Route::post('{user}', [ProfileController::class, 'updateProfile']);
-        Route::post('{user}/password', [ProfileController::class, 'updatePassword']);
-    });
-
-    // Report
-    Route::post('report/{category}', [ReportController::class, 'store']);
 });
--- routes/auth.php
+++ routes/auth.php
@@ -9,36 +9,76 @@ use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/register', [RegisteredUserController::class, 'store'])
-    ->middleware('guest')
-    ->name('register');
+// v1
+// Route::prefix('v1')->group(function () {
+//     Route::post('/register', [RegisteredUserController::class, 'store'])
+//         ->middleware('guest')
+//         ->name('register');
 
-Route::post('/login', [AuthenticatedSessionController::class, 'store'])
-    ->middleware('guest')
-    ->name('login');
+//     Route::post('/login', [AuthenticatedSessionController::class, 'store'])
+//         ->middleware('guest')
+//         ->name('login');
 
-Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])
-    ->middleware('guest')
-    ->name('password.email');
+//     Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])
+//         ->middleware('guest')
+//         ->name('password.email');
 
-Route::post('/reset-password', [NewPasswordController::class, 'store'])
-    ->middleware('guest')
-    ->name('password.store');
+//     Route::post('/reset-password', [NewPasswordController::class, 'store'])
+//         ->middleware('guest')
+//         ->name('password.store');
 
-Route::get('/verify-email/{id}/{hash}', VerifyEmailController::class)
-    ->middleware(['auth', 'signed', 'throttle:6,1'])
-    ->name('verification.verify');
+//     Route::get('/verify-email/{id}/{hash}', VerifyEmailController::class)
+//         ->middleware(['auth', 'signed', 'throttle:6,1'])
+//         ->name('verification.verify');
 
-Route::post('/email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
-    ->middleware(['auth', 'throttle:6,1'])
-    ->name('verification.send');
+//     Route::post('/email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
+//         ->middleware(['auth', 'throttle:6,1'])
+//         ->name('verification.send');
 
-Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
-    ->middleware('auth:sanctum')
-    ->name('logout');
+//     Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
+//         ->middleware('auth:sanctum')
+//         ->name('logout');
 
-Route::get('/user', function (Request $request) {
-    return response()->json([
-        'user' => $request->user(),
-    ]);
-})->middleware('auth:sanctum')->name('user');
+//     Route::get('/user', function (Request $request) {
+//         return response()->json([
+//             'user' => $request->user(),
+//         ]);
+//     })->middleware('auth:sanctum')->name('user');
+// });
+
+// TESTING
+Route::prefix('testing')->group(function () {
+    Route::post('/register', [RegisteredUserController::class, 'store'])
+        ->middleware('guest')
+        ->name('register');
+
+    Route::post('/login', [AuthenticatedSessionController::class, 'store'])
+        ->middleware('guest')
+        ->name('login');
+
+    Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])
+        ->middleware('guest')
+        ->name('password.email');
+
+    Route::post('/reset-password', [NewPasswordController::class, 'store'])
+        ->middleware('guest')
+        ->name('password.store');
+
+    Route::get('/verify-email/{id}/{hash}', VerifyEmailController::class)
+        ->middleware(['auth', 'signed', 'throttle:6,1'])
+        ->name('verification.verify');
+
+    Route::post('/email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
+        ->middleware(['auth', 'throttle:6,1'])
+        ->name('verification.send');
+
+    Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
+        ->middleware('auth:sanctum')
+        ->name('logout');
+
+    Route::get('/user', function (Request $request) {
+        return response()->json([
+            'user' => $request->user(),
+        ]);
+    })->middleware('auth:sanctum')->name('user');
+});
